b3e1e89af7cca0ea217291f0d4076fbfbec6ae22
TotalSimplicity <68793354+TotalSimplicity@users.noreply.github.com>
2023-03-02 15:50:37 -0500

    Fix config

---
+++ config.py
@@ -0,0 +1,29 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+api = os.getenv('OPENAI-APIKEY')
+
+#spotify bot info
+spotify_client_id = os.getenv('spotify_client_id')
+spotify_client_secret = os.getenv('spotify_client_secret')
+
+#aws tts info
+aws_access_key_id = os.getenv('aws_access_key_id')
+aws_secret_key = os.getenv('aws_secret_key')
+
+good_file = open("goodpersonality.txt", "r")
+good = good_file.read()
+good_file.close()
+
+instructs_file = open("instructs.txt", "r")
+instructs = instructs_file.read()
+instructs_file.close()
+
+conversation_file = open("midpersonality.txt", "r")
+conversation = conversation_file.read()
+conversation_file.close()
+
+debate_file = open("debatething.txt", "r")
+debatething = debate_file.read()
+debate_file.close()
