03586a4da5110c6bf43ea9ba970b6225400ae8ec
TotalSimplicity <68793354+TotalSimplicity@users.noreply.github.com>
2023-03-02 16:17:51 -0500

    Bug fixes

--- gui.py
+++ gui.py
@@ -8,9 +8,9 @@ aiChoice = easygui.buttonbox("Choose an AI type.", choices=aiChoices)
 
 # Set the voice that each type is meant for
 if aiChoice == "Assistant" or aiChoice == "Conversationalist":
-   ao.set_voice("british")
+   ao.voice_id = ao.set_voice("british")
 elif aiChoice == "DebateThing":
-   ao.set_voice("indian")
+   ao.voice_id = ao.set_voice("indian")
 
 # Ask if the user wants to change the voice
 inputChoices = ["Yes", "No"]
@@ -21,11 +21,11 @@ if inputChoice == "Yes":
    voiceChoices = ["British", "Indian"]
    voiceChoice = easygui.buttonbox("Do you want to override the default voice for your chosen AI?", choices=inputChoices)
    if voiceChoice == "British":
-      ao.set_voice("british")
+      ao.voice_id = ao.set_voice("british")
    elif voiceChoice == "Indian":
-      ao.set_voice("indian")
+      ao.voice_id = ao.set_voice("indian")
       
- finalMessage = r"Starting {aiChoice}......"
+finalMessage = f"Starting {aiChoice}......"
 
 # Display the user's answers
 easygui.msgbox(finalMessage, ok_button="Start")
